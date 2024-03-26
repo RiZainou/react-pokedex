3ca540a8d49f864e931cee66a4ba8b755dd44c7e
toun <m.toun976@gmail.com>
2024-03-26 15:18:11 +0100

    mew mew

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,10 +1,35 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
+
 
 function PokemonCard() {
+
+  const pokemon = pokemonList[0];
   return (
-    <figure> <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-    <figcaption>Bulbasaur</figcaption>
+    <>
+    <figure> 
+      <>{pokemon.imgSrc === undefined ? <p>???</p> : <img src= {pokemon.imgSrc} alt="Bulbasaur" /> }</>
+    <figcaption>{pokemon.name}</figcaption>
     </figure>
+    
+    </>
+  
   );
+
+
+  
+
 }
+
+
 
 export default PokemonCard;
