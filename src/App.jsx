67676c36a1e4f60cbb5cdc660a502e0e78862ac0
toun <m.toun976@gmail.com>
2024-03-27 16:35:38 +0100

    ajoute state 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import PokemonCard from "./components/PokemonCard" 
 import './App.css'
+import { useState } from "react";
 
 const pokemonList = [
   {
@@ -8,18 +9,49 @@ const pokemonList = [
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
   },
   {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+  },
+  {
     name: "mew",
   },
 ];
 
 function App() {
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  
+  const plusButton = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  }
+  
+  const prevButton = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  }
+ 
+ 
   return (
     <div>
-      <PokemonCard pokemon = {pokemonList[0]}/>
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      {pokemonIndex > 0 ? <button onClick={prevButton}>Précédent</button>  : null } 
+      {pokemonIndex < pokemonList.length - 1 ? <button onClick={plusButton}>Suivant</button> : null}
+      
+      
       
     </div>
     
-  )
+  );
     
   }     
  
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,13 +1,3 @@
-// const pokemonList = [
-//   {
-//     name: "bulbasaur",
-//     imgSrc:
-//       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-//   },
-//   {
-//     name: "mew",
-//   },
-// ];
 
 import PropTypes from "prop-types";
 
